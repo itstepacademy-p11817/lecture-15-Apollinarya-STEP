2c71fb956d31aa60b89dc494c15c03fbed9efaa6
Polina Manenok <Apollinarya-STEP@users.noreply.github.com>
2017-07-24 23:38:12 +0300

    Done
    .

--- task_1.c
+++ task_1.c
@@ -1,17 +1,41 @@
 #include "task_1.h"
 
 // Function counting sum of digits in number given
-long long get_digits_sum(long long number) {
+long long get_digits_sum(long long number) 
+{
 
-    // Your code
+	int sum = 0;
+	while (number > 0)
+	{
+		sum += number % 10;
+		number /= 10;
+	}
 
-    return 0;
+	return sum;
 }
 
 // Function counting sum of digits for each prime number in range (l,r)
-long long counter(long long l, long long r) {
-
-    // Your code
-
-    return 0;
+long long counter(long long l, long long r) 
+{
+	long long sum_of_digits = 0;
+	for (long long i = l + 1; i < r; i++)
+	{
+		int count = 0;
+		for (long long i1 = 1; i1 <= i; i1++)
+		{
+			if (i%i1 == 0)
+			{
+				count++;
+			}
+			if (count > 2)
+			{
+				break;
+			}
+		}
+		if (count == 2)
+		{
+			sum_of_digits += get_digits_sum(i);
+		}
+	}
+	return sum_of_digits;
 }
